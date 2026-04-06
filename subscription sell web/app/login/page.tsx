b0fb6e183af1d8c4eda526599a2ab4299b931fdb
Ryuyjg <anshifkpmuhammad140@gmail.com>
2026-04-06 18:33:47 +0530

    Remove duplicate nested app to stabilize Vercel deploys

--- subscription sell web/app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import { AuthCard } from "@/components/auth/auth-card";
-
-export default function LoginPage() {
-  return (
-    <div className="container flex min-h-[calc(100vh-10rem)] items-center justify-center py-16">
-      <AuthCard />
-    </div>
-  );
-}
