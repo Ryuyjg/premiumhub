a52a3bd0fdf9b69c9682e8dffe2e6cf530859d9d
Ryuyjg <anshifkpmuhammad140@gmail.com>
2026-09-03 14:48:32 +0530

    Updates

---
+++ _next/static/MBD80MzFShAVd-5_lbepQ/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fapi\u002Fsubscriptions\u002F[id]\u002Fcredentials","\u002Fproducts\u002F[slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
---
+++ _next/static/chunks/app/refund-policy/page-d40b66a51574c634.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[977,1366,1485,7066,7220,7505,9109,9877],{2665:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,2619,23))}},e=>{e.O(0,[2619,8441,1255,7358],()=>e(e.s=2665)),_N_E=e.O()}]);
